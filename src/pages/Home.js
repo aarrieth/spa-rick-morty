e302f516a750475d353c097fe0d61d91a5148465
aarrieth <alfonsoluisarrieta253@gmail.com>
2020-12-16 02:43:18 -0500

    [create template fro Home page]

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+const Home = () => {
+    const view = `
+        <div class="Characters">
+            <div class="Characters-item">
+                <a href="#/1/" alt="name character">
+                    <h2>Name</h2>
+                </a>
+            </div>
+        </div>
+    `;
+    return view;
+};
+
+export default Home;
